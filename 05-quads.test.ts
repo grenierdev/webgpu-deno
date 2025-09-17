7d511c7ea8be67db2b26eb9bcf386701ba58aaa7
Michael Grenier <michael@grenier.dev>
2025-09-17 09:46:09 -0400

    feat: quads scale and rotation

--- 05-quads.test.ts
+++ 05-quads.test.ts
@@ -1,26 +1,28 @@
 import { createCapture } from "@std/webgpu/create-capture";
 import { getRowPadding } from "@std/webgpu/row-padding";
 import { assertSnapshot } from "@std/testing/snapshot";
-import { createBufferWithContents, writeBufferToPNG } from "./utils.ts";
+import { createBufferWithContents } from "./utils.ts";
 
 Deno.test("instancing quads", async (t) => {
 	const adapter = await navigator.gpu.requestAdapter();
 	const device = await adapter?.requestDevice()!;
 
 	const dimensions = {
-		width: 512,
-		height: 512,
+		width: 32,
+		height: 32,
 	};
 
-	// vec2(top-left), vec2(bottom-right), rotation
+	// vec2(center), vec2(size), rotation
 	const quadsBuffer = createBufferWithContents(device, {
 		size: 10 * 10 * 8 * Float32Array.BYTES_PER_ELEMENT,
 		usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
 		// deno-fmt-ignore
 		contents: new Float32Array([
-			-1.0, 1.0, 0.0, 0.0, 0.0,
-			0.0, 0.0, 1.0, -1.0, Math.PI / 2,
-			-0.5, 0.5, 0.5, -0.5, Math.PI / 10,
+			-0.5, 0.5, 0.5, 0.5, 0.0,
+			0.5, 0.5, -0.5, 0.5, 0.0,
+			-0.5, -0.5, 0.5, -0.5, 0.0,
+			0.5, -0.5, -0.5, -0.5, 0.0,
+			0.0, 0.0, 0.5, 0.5, Math.PI / 10,
 		]),
 	});
 
@@ -32,8 +34,8 @@ Deno.test("instancing quads", async (t) => {
 			};
 
 			struct InstanceInput {
-				@location(5) TopLeftPosition: vec2<f32>,
-				@location(6) BottomRightPosition: vec2<f32>,
+				@location(5) Center: vec2<f32>,
+				@location(6) Size: vec2<f32>,
 				@location(7) Rotation: f32,
 			};
 
@@ -44,23 +46,27 @@ Deno.test("instancing quads", async (t) => {
 
 			@vertex
 			fn vs_main(vert: VertexInput, inst: InstanceInput) -> VertexOutput {
-				var pos = vec4<f32>(
-					mix(inst.TopLeftPosition.x, inst.BottomRightPosition.x, f32(vert.Index & 1u)),
-					mix(inst.TopLeftPosition.y, inst.BottomRightPosition.y, f32(vert.Index < 2u)),
+				var p = vec2<f32>(
+					f32(vert.Index & 1u),
+					f32(vert.Index < 2u)
+				);
+				var v = vec4<f32>(
+					mix(-1.0, 1.0, p.x),
+					mix(1.0, -1.0, p.y),
 					0.0,
 					1.0,
 				);
-				var rot = mat4x4<f32>(
-					cos(inst.Rotation), sin(inst.Rotation), 0.0, 0.0,
-					-sin(inst.Rotation), cos(inst.Rotation), 0.0, 0.0,
+				var t = mat4x4<f32>(
+					cos(inst.Rotation) * inst.Size.x, sin(inst.Rotation), 0.0, inst.Center.x,
+					-sin(inst.Rotation), cos(inst.Rotation) * inst.Size.y, 0.0, inst.Center.y,
 					0.0, 0.0, 1.0, 0.0,
 					0.0, 0.0, 0.0, 1.0,
 				);
 				var out: VertexOutput;
-				out.Position = pos * rot;
+				out.Position = v * t;
 				out.UV = vec2<f32>(
-					mix(0.0, 1.0, f32(vert.Index & 1u)),
-					1. - mix(1.0, 0.0, f32(vert.Index < 2u))
+					mix(0.0, 1.0, p.x),
+					1. - mix(1.0, 0.0, p.y)
 				);
 				return out;
 			}
@@ -116,7 +122,7 @@ Deno.test("instancing quads", async (t) => {
 		},
 		primitive: {
 			topology: "triangle-strip",
-			cullMode: "back",
+			// cullMode: "back",
 		},
 	});
 
@@ -135,7 +141,7 @@ Deno.test("instancing quads", async (t) => {
 	});
 	renderPass.setPipeline(renderPipeline);
 	renderPass.setVertexBuffer(0, quadsBuffer);
-	renderPass.draw(4, 3, 0, 0);
+	renderPass.draw(4, 5, 0, 0);
 	renderPass.end();
 
 	commandEncoder.copyTextureToBuffer(
@@ -156,6 +162,4 @@ Deno.test("instancing quads", async (t) => {
 	outputBuffer.unmap();
 
 	await assertSnapshot(t, outputArrayBuffer);
-
-	await writeBufferToPNG(outputArrayBuffer, "05-quads.png", dimensions);
 });
